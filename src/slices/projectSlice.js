a8b805e01a63b6aea429ce9f5b08237a76afeb54
Oktay Aydoğan <aydoganooktay@gmail.com>
2024-05-12 15:33:12 +0300

    Update projectSlice.js with automation company selection for Montaj Bankoları

--- src/slices/projectSlice.js
+++ src/slices/projectSlice.js
@@ -9,7 +9,7 @@ import ResultScreen from "../screens/ResultScreen";
 import ValuesScreen from "../screens/ValuesScreen";
 
 const initialState = {
-	projectName: "",
+	projectName: "Montaj Bankoları İçin Otomasyon Firması Seçimi ",
 	currentStep: 0,
 	screens: [
 		{
@@ -49,27 +49,31 @@ const initialState = {
 	matrix: {},
 	/*
 	alternatives: {
-		0: "Ali",
-		1: "Baha",
-		2: "Can",
-		3: "Derya",
-		4: "Emel",
-		5: "Fatma",
+		0: "Promer",
+		1: "Yüksek İnovasyon",
+		2: "Prolojik",
+		3: "Mertay",
+		4: "3E Otomasyon",
+		5: "Rovimek",
 	},
 	criterias: {
-		0: { criteria: "İngilizce", weight: 20, type: "+" },
-		1: { criteria: "Not Ort.", weight: 30, type: "+" },
-		2: { criteria: "Üniv.", weight: 20, type: "+" },
-		3: { criteria: "Tavsiye", weight: 10, type: "+" },
-		4: { criteria: "Mülakat", weight: 20, type: "+" },
+		0: { criteria: "Güvenlik", weight: 30, type: "+" },
+		1: { criteria: "Destek ve Bakım Hizmetleri", weight: 19, type: "+" },
+		2: { criteria: "Entegrasyon ve Uyum Yetenekleri", weight: 18, type: "+" },
+		3: {
+			criteria: "Referanslar ve Müşteri Geri Bildirimleri",
+			weight: 14,
+			type: "+",
+		},
+		4: { criteria: "Teknoloji ve Altyapı", weight: 19, type: "+" },
 	},
 	matrix: {
-		0: { 0: 69, 1: 3.1, 2: 9, 3: 7, 4: 4 },
-		1: { 0: 59, 1: 3.9, 2: 7, 3: 6, 4: 10 },
-		2: { 0: 60, 1: 3.6, 2: 8, 3: 8, 4: 7 },
-		3: { 0: 62, 1: 3.8, 2: 7, 3: 10, 4: 6 },
-		4: { 0: 70, 1: 2.8, 2: 10, 3: 6, 4: 6 },
-		5: { 0: 65, 1: 4, 2: 9, 3: 8, 4: 8 },
+		0: { 0: 10, 1: 8, 2: 7, 3: 7, 4: 8 },
+		1: { 0: 10, 1: 7, 2: 7, 3: 8, 4: 7 },
+		2: { 0: 9, 1: 8, 2: 5, 3: 6, 4: 6 },
+		3: { 0: 9, 1: 6, 2: 6, 3: 9, 4: 7 },
+		4: { 0: 9, 1: 8, 2: 6, 3: 7, 4: 6 },
+		5: { 0: 8, 1: 7, 2: 7, 3: 6, 4: 6 },
 	},*/
 };
 
